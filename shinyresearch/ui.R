9bf82e5d88d2a710cc6679240e882a50c3b0dc46
EcoFire <justin.ziegler@colostate.edu>
2018-10-04 00:55:13 -0600

    shiny research app

--- shinyresearch/ui.R
+++ shinyresearch/ui.R
@@ -1,19 +1,9 @@
 library(shiny)
 
-# Define UI for dataset viewer application
-shinyUI(pageWithSidebar(
-  # Sidebar with controls to select a dataset and specify the number
-  # of observations to view
-  sidebarPanel(
-    selectInput("researchdata", "Choose a topic:", 
-                choices = c("fire behavior", "fire ecology", "spatial forest structure", "forest management", "forest dynamics"))
-  ),
-
-  # Show a summary of the dataset and an HTML table with the requested
-  # number of observations
-  mainPanel(
-    verbatimTextOutput("summary"),
-
-    tableOutput("view")
-  )
+shinyUI(fluidPage(
+  #User dropbox
+  selectInput("tag", "Choose a topic:", 
+              choices = c("fire behavior", "fire ecology", "spatial forest structure", "forest management", "forest dynamics"))
+  #Print table to UI
+  ,tableOutput("table")
 ))
